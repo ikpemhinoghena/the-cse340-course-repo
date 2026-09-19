6e7a10a2b93b9cd44d74893b43d2df3144ccaa3b
Ikpemhinoghena Etiuzale <etiuzaleikpemhinoghena@gmail.com>
2026-09-19 18:07:38 +0100

    dkdmcnd

--- src/controllers/categories.js
+++ src/controllers/categories.js
@@ -1,4 +1,4 @@
-import { getAllCategories } from '../models/categories.js';
+import { getAllCategories, getCategoryDetails, getProjectsByCategoryId } from '../models/categories.js';
 
 const showCategoriesPage = async (req, res) => {
     const categories = await getAllCategories();
@@ -6,4 +6,12 @@ const showCategoriesPage = async (req, res) => {
     res.render('categories', { title, categories });
 };
 
-export { showCategoriesPage };
+const showCategoryDetailsPage = async (req, res) => {
+    const categoryId = req.params.id;
+    const categoryDetails = await getCategoryDetails(categoryId);
+    const projects = await getProjectsByCategoryId(categoryId);
+    const title = 'Category Details';
+    res.render('category', { title, categoryDetails, projects });
+};
+
+export { showCategoriesPage, showCategoryDetailsPage };
--- src/models/categories.js
+++ src/models/categories.js
@@ -9,4 +9,43 @@ const result = await db.query(query);
 return result.rows;
 }
 
-export {getAllCategories}
+const getCategoryDetails = async (id) => {
+const query = `SELECT category_id, name
+        FROM public.category
+        WHERE category_id = $1;
+    `;
+const queryParams = [id];
+const result = await db.query(query, queryParams);
+
+return result.rows.length > 0 ? result.rows[0] : null;
+};
+
+const getCategoriesByProjectId = async (projectId) => {
+const query = `SELECT category.category_id, category.name
+        FROM public.project_category
+        INNER JOIN public.category
+        ON project_category.category_id = category.category_id
+        WHERE project_category.project_id = $1
+        ORDER BY category.name;
+    `;
+const queryParams = [projectId];
+const result = await db.query(query, queryParams);
+
+return result.rows;
+};
+
+const getProjectsByCategoryId = async (categoryId) => {
+const query = `SELECT service_project.project_id, service_project.title
+        FROM public.project_category
+        INNER JOIN public.service_project
+        ON project_category.project_id = service_project.project_id
+        WHERE project_category.category_id = $1
+        ORDER BY service_project.date;
+    `;
+const queryParams = [categoryId];
+const result = await db.query(query, queryParams);
+
+return result.rows;
+};
+
+export {getAllCategories, getCategoryDetails, getCategoriesByProjectId, getProjectsByCategoryId}
