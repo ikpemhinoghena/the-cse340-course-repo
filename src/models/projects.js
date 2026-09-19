196f971b551bd1d763454ec326c7e593eac2dd2b
Ikpemhinoghena Etiuzale <etiuzaleikpemhinoghena@gmail.com>
2026-09-19 17:47:05 +0100

    Complete W03 route parameters activity

--- src/models/projects.js
+++ src/models/projects.js
@@ -13,4 +13,16 @@ const result = await db.query(query);
 return result.rows;
 }
 
-export {getAllProjects}
+const getProjectsByOrganizationId = async (organizationId) => {
+const query = `SELECT project_id, organization_id, title, description, location, date
+        FROM service_project
+        WHERE organization_id = $1
+        ORDER BY date;
+    `;
+const queryParams = [organizationId];
+const result = await db.query(query, queryParams);
+
+return result.rows;
+};
+
+export {getAllProjects, getProjectsByOrganizationId}
