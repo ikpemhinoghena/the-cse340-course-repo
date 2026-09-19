81eb3eec906eaa8e1e065a428e48ee0aa47b20ad
Ikpemhinoghena Etiuzale <etiuzaleikpemhinoghena@gmail.com>
2026-09-19 17:21:42 +0100

    Complete W03 middleware activity

--- server.js
+++ server.js
@@ -30,6 +30,18 @@ app.set('view engine', 'ejs');
 // Tell Express where to find your templates
 app.set('views', path.join(__dirname, 'src/views'));
 
+app.use((req, res, next) => {
+    if (NODE_ENV === 'development') {
+        console.log(`${req.method} ${req.url}`);
+    }
+    next();
+});
+
+app.use((req, res, next) => {
+    res.locals.NODE_ENV = NODE_ENV;
+    next();
+});
+
 /**
  * Routes
  */
